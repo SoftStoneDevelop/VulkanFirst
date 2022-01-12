4b1fb965c66e2fe0d6c68c926a5fbc407cc191ef
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-01-12 05:26:00 +0300

    feat: recompile shaders automatically when build solution

--- compile.bat
+++
@@ -1,3 +0,0 @@
-C:/VulkanSDK/1.2.198.1/Bin/glslc.exe ShadersSource/simple_shader.vert -o ShadersCompile/simple_shader.vert.spv
-C:/VulkanSDK/1.2.198.1/Bin/glslc.exe ShadersSource/simple_shader.frag -o ShadersCompile/simple_shader.frag.spv
-pause
