de30cabe2e8f4859e0ce43a4551c1573aa794fea
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-04-24 03:43:48 +0300

    Revert "Tutorial 12"
    This reverts commit 3b6ebcf13632e50b7f17da11a2919537e59a0db2.

--- simple_renderer_system.cpp
+++ simple_renderer_system.cpp
@@ -12,7 +12,8 @@
 namespace lve {
 
 	struct SimplePushConstantData {
-		glm::mat4 transform{ 1.f };
+		glm::mat2 transform{ 1.f };
+		glm::vec2 offset;
 		alignas(16) glm::vec3 color;
 	};
 
@@ -62,16 +63,21 @@ namespace lve {
 	}
 
 	void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::vector<LveGameObject>& gameObjects) {
+		int i = 0;
+		for (auto& obj : gameObjects)
+		{
+			i += 1;
+			obj.transform2d.rotation = glm::mod<float>(obj.transform2d.rotation + 0.001f * i, 2.f * glm::pi<float>());
+		}
+
 		lvePipeline->bind(commandBuffer);
 
 		for (auto& obj : gameObjects)
 		{
-			obj.transform.rotation.y = glm::mod<float>(obj.transform.rotation.y + 0.0001f, glm::two_pi<float>());
-			obj.transform.rotation.x = glm::mod<float>(obj.transform.rotation.x + 0.0005f, glm::two_pi<float>());
-
 			SimplePushConstantData push{};
+			push.offset = obj.transform2d.translation;
 			push.color = obj.color;
-			push.transform = obj.transform.mat4();
+			push.transform = obj.transform2d.mat2();
 
 			vkCmdPushConstants(
 				commandBuffer,
