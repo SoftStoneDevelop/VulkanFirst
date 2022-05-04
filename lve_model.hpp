f19356cff562b904fbda8386991f8ce6425106b5
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-05-04 15:45:14 +0300

    feat: Tutorial 17

--- lve_model.hpp
+++ lve_model.hpp
@@ -8,6 +8,7 @@
 #include <glm/glm.hpp>
 
 //std
+#include <memory>
 #include <vector>
 
 namespace lve {
@@ -17,16 +18,28 @@ namespace lve {
 
 		struct  Vertex
 		{
-			glm::vec3 position;
-			glm::vec3 color;
+			glm::vec3 position{};
+			glm::vec3 color{};
+			glm::vec3 normal{};
+			glm::vec2 uv{};
 
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+
+			bool operator==(const Vertex& other) const {
+				return 
+					position == other.position && 
+					color == other.color && 
+					normal == other.normal && 
+					uv == other.uv;
+			}
 		};
 
 		struct Builder {
 			std::vector<Vertex> vertices{};
 			std::vector<uint32_t> indices{};
+
+			void loadModel(const std::string& filepath);
 		};
 
 		LveModel(LveDevice& lveDevice, const LveModel::Builder& builder);
@@ -35,6 +48,8 @@ namespace lve {
 
 		LveModel(const LveModel&) = delete;
 		void operator=(const LveModel&) = delete;
+
+		static std::unique_ptr<LveModel> createModelFromFile(LveDevice& device, const std::string& filepath);
 
 		void bind(VkCommandBuffer commandBuffer);
 		void draw(VkCommandBuffer commandBuffer);
