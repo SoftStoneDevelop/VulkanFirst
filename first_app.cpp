e43d10f99ce40a796d4ce59ba799b7f006eaa3b7
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-05-07 16:18:30 +0300

    feat: Tutorial 18

--- first_app.cpp
+++ first_app.cpp
@@ -55,16 +55,21 @@ namespace lve {
 		}
 
 		vkDeviceWaitIdle(lveDevice.device());
-	}
+	} 
 
 	void FirstApp::loadGameObjects() {
-		std::shared_ptr<LveModel> lveModel = LveModel::createModelFromFile(lveDevice, "E:/Work/VulkanFirst3d/Models/smooth_vase.obj");
+		std::shared_ptr<LveModel> lveModel = LveModel::createModelFromFile(lveDevice, "E:/Work/VulkanFirst3d/Models/flat_vase.obj");
+        auto flatVase = LveGameObject::createGameObject();
+		flatVase.model = lveModel;
+		flatVase.transform.translation = { -.5f, .5f, 2.5f };
+		flatVase.transform.scale = { 3.f, 1.5f, 3.f };
+        gameObjects.push_back(std::move(flatVase));
 
-        auto gameObj = LveGameObject::createGameObject();
-		gameObj.model = lveModel;
-		gameObj.transform.translation = { .0f, .0f, 2.5f };
-		gameObj.transform.scale = glm::vec3(3.f);
-
-        gameObjects.push_back(std::move(gameObj));
+		lveModel = LveModel::createModelFromFile(lveDevice, "E:/Work/VulkanFirst3d/Models/smooth_vase.obj");
+		auto smoothVase = LveGameObject::createGameObject();
+		smoothVase.model = lveModel;
+		smoothVase.transform.translation = { .5f, .5f, 2.5f };
+		smoothVase.transform.scale = { 3.f, 1.5f, 3.f };
+		gameObjects.push_back(std::move(smoothVase));
 	}
 }
